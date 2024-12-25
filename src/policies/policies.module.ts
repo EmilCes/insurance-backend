58da14fe6aa5453049717fc92e8e4890713de15f
sulem <sulem477@gmail.com>
2024-12-24 18:49:06 -0600

    feat: Crear poliza

--- src/policies/policies.module.ts
+++ src/policies/policies.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PoliciesService } from './policies.service';
 import { PoliciesController } from './policies.controller';
 import { PrismaService } from 'src/prisma.service';
+import { VehiclesService } from 'src/vehicles/vehicles.service';
 
 @Module({
   controllers: [PoliciesController],
-  providers: [PoliciesService, PrismaService],
+  providers: [PoliciesService, PrismaService, VehiclesService],
 })
 export class PoliciesModule {}
--- src/policies/policies.service.ts
+++ src/policies/policies.service.ts
@@ -1,44 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
 import { CreatePolicyDto } from './dto/create-policy.dto';
 import { UpdatePolicyDto } from './dto/update-policy.dto';
 import { PrismaService } from 'src/prisma.service';
 import { Prisma } from '@prisma/client';
+import { VehiclesService } from 'src/vehicles/vehicles.service';
 
 @Injectable()
 export class PoliciesService {
   constructor(
-    private prisma: PrismaService
+    private prisma: PrismaService,
+    private vehicleService: VehiclesService
   ) { }
 
   async create(createPolicyDto: CreatePolicyDto) {
     //Recuperar usuario
-    //primero crear el coche
+    
+    const plates = await this.vehicleService.validatePlates(createPolicyDto.plates);
+    if (plates) {
+      throw new ConflictException("Plates found");
+    }
 
+    const serviceVehicle = await this.prisma.serviceVehicle.findUnique({ where: { idService: createPolicyDto.idService } })
+    const typeVehicle = await this.prisma.type.findUnique({ where: { idType: createPolicyDto.idType } })
+    const modelVehicle = await this.prisma.model.findUnique({ where: { idModel: createPolicyDto.idModel, idBrand: createPolicyDto.idBrand } })
 
-    //verificar si es vigente
-    const policyPlan = await this.prisma.policyPlan.findUnique({ where: { idPolicyPlan : createPolicyDto.idPolicyPlan }});
-    if(!policyPlan){
-      return null;
+    if (!serviceVehicle || !typeVehicle || !modelVehicle) {
+      throw new BadRequestException("Vehicle data not correct");
+    }
+
+    const policyPlan = await this.prisma.policyPlan.findUnique({
+      where: { idPolicyPlan: createPolicyDto.idPolicyPlan },
+      include: { Service: true }
+    });
+
+    if (!policyPlan) {
+      throw new BadRequestException("Policy plan doesnt exists");
+    }
+
+    let policyCreated = null;
+
+    try {
+      await this.prisma.$transaction(async (prisma) => {
+        const policyVehicle = await this.prisma.vehicle.create({
+          data: {
+            plates: createPolicyDto.plates,
+            serialNumberVehicle: createPolicyDto.series,
+            occupants: createPolicyDto.occupants,
+            idService: createPolicyDto.idService,
+            idType: createPolicyDto.idType,
+            idModel: createPolicyDto.idModel,
+            idColor: createPolicyDto.idColor
+          }
+        });
+
+        const policy = await this.prisma.policy.create({
+          data: {
+            period: createPolicyDto.perMonthsPayment,
+            isCanceled: false,
+            coveredCost: new Prisma.Decimal(+policyPlan.basePrice * createPolicyDto.yearOfPolicy),
+            startDate: new Date(),
+            planTitle: policyPlan.title,
+            planDescription: policyPlan.description,
+            idPolicyPlan: policyPlan.idPolicyPlan,
+            plates: createPolicyDto.plates,
+            idUser: 1
+          },
+        });
+
+        for (let index = 0; index < policyPlan.Service.length; index++) {
+          const service = policyPlan.Service[index];
+          const policyService = await this.prisma.policyService.create({
+            data: {
+              name: service.name,
+              isCovered: service.isCovered,
+              coveredCost: service.coveredCost,
+              serialNumber: policy.serialNumber
+            },
+          });
+        }
+        policyCreated = policy;
+      })
+    } catch (err) {
+      throw new BadRequestException("Error creating the policy");
     }
     
-    const policy = await this.prisma.policy.create({
-      data: {
-        period: createPolicyDto.perMonthsPayment, 
-        isCanceled: false, 
-        coveredCost: new Prisma.Decimal(+policyPlan.basePrice * createPolicyDto.yearOfPolicy), 
-        startDate: new Date(), 
-        planTitle: policyPlan.title, 
-        planDescription: policyPlan.description, 
-        idPolicyPlan: policyPlan.idPolicyPlan, 
-        plates: createPolicyDto.plates, 
-        idUser: 1
-      },
-    });
-    
-
-
-
-    return 'This action adds a new policy';
+    return policyCreated;
   }
 
   findAll() {
